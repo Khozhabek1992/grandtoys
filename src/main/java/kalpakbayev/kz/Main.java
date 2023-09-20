26612c7280d012ba3c940acdff162026d3bf378d
Kalpakbaev <kozha_1992@mail.ru>
2023-09-20 06:49:02 +0500

    geneder

--- src/main/java/kalpakbayev/kz/Main.java
+++ src/main/java/kalpakbayev/kz/Main.java
@@ -1,5 +1,13 @@
-package kalpakbayev.kz;public class Main {
+package kalpakbayev.kz;
+
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int a = 15;
+        int b = 30;
+        int c = a + b;
+        System.out.println(c);
     }
 }
---
+++ src/main/java/kalpakbayev/kz/Toymaga.java
@@ -0,0 +1,20 @@
+package kalpakbayev.kz;
+
+public class Toymaga {
+    String name;
+
+    public Toymaga(String name) {
+        this.name = name;
+    }
+
+    String color;
+    int summa;
+
+    public int getSumma() {
+        return summa;
+    }
+
+    public void setSumma(int summa) {
+        this.summa = summa;
+    }
+}
